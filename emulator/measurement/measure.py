3ed586d9de40ea9f552bc83a01ba4db9acdbff53
shenyunbin <shenyunbin@outlook.com>
2021-07-03 22:27:45 +0200

    measure add arr to str

--- emulator/measurement/measure.py
+++ emulator/measurement/measure.py
@@ -1,46 +1,31 @@
 import time
 import json
+import pickle
 
-
-def measure_write(filename,times):
+def measure_write(filename,contents):
     try:
         f = open("measurement/"+filename+".csv","r")
         data = f.read()
     except Exception:
         data = ''
-    data = data + ','.join(map(str,times)) + ',\n'
+    data = data + ','.join(map(str,contents)) + ',\n'
     with open("measurement/"+filename+".csv",'w') as f:
         f.write(data)
     f.close()
 
+def measure_arr_to_jsonstr(numpy_arr):
+    return json.dumps([[y for y in x] for x in numpy_arr], separators=('|', ':'))
 
-# def measure_time(marker,init_settings,time=time.time()):
-#     if '@t' in init_settings:
-#         init_settings['@t']+=[time]
-#     else:
-#         init_settings['@t']=[time]
-#     if '@m' in init_settings:
-#         init_settings['@m']+=[marker]
-#     else:
-#         init_settings['@m']=[marker]
+def measure_jsonstr_to_arr(jsonstr):
+    json.loads(jsonstr.replace('|',','))
 
-# def measure_time_save(init_settings):
-#     x0 = init_settings['@t'][0]
-#     init_settings['@t'] = [str(x-x0) for x in init_settings['@t']]
-#     f = open("measure/measure.csv","r")
-#     data = f.read()
-#     data = data + ','.join(init_settings['@m']) + ',\n'
-#     data = data + ','.join(init_settings['@t']) + ',\n'
+def measure_read_csv_to_2dlist(filename):
+    f = open("measurement/"+filename+".csv",'r')
+    lines = f.readlines()
+    return [line.split(',') for line in lines]
 
-#     with open('measure/measure.csv','w') as f:
-#         f.write(data)
-#     f.close()
 
-    # arr = json.load(open('measure/measure.json', 'r'))
-    # dct = {}
-    # x0 = init_settings['@t'][0]
-    # init_settings['@t'] = [x-x0 for x in init_settings['@t']]
-    # for key,value in zip(init_settings['@m'],init_settings['@t']):
-    #     dct[key] = value
-    # arr.append(dct)
-    # json.dump(arr,open('measure/measure.json', 'w'))
+
+if __name__ == '__main__':
+    #table1 = measure_read_csv_to_2dlist(filename)
+    pass
